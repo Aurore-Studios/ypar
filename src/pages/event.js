e91718e0fe89e8c5c76ccae1037415d32368a102
awsame303 <72770606+awsame303@users.noreply.github.com>
2023-02-05 10:21:04 -0500

    placeholder for events and faq

---
+++ src/pages/event.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Layout from '../../components/Layout'
+import Link from 'next/link'
+
+export default function Events() {
+    return (
+        <>
+            <Layout>
+                <p className={'text-pink-500'}> placeholder </p>
+            </Layout>
+        </>
+    )
+}
