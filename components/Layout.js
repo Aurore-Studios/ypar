d27f49f55d80ae859b7cbf476c6430284ca702bb
awsame303 <72770606+awsame303@users.noreply.github.com>
2023-02-12 09:49:15 -0500

    faq.js organized and made
    Signed-off-by: awsame303 <72770606+awsame303@users.noreply.github.com>

--- components/Layout.js
+++ components/Layout.js
@@ -61,77 +61,94 @@ export default function Layout({ children, home }) {
         <meta name="twitter:card" content="summary_large_image" />
         <title>WWMS Now</title>
       </Head>
-        <div
-          className={`w-full sticky top-0 min-h-screen`}
-        >
-            <div className='flex items-center hover:cursor-default w-full bg-pink-100 py-3 lg:px-16 md:px-8 px-4 border-b-2 border-pink-500'>
-                <Link href='/'>
-                    <img src='/logo.png' alt="logo" width="120" className='mr-1 rounded border border-pink-500 hover:scale-[1.05] transition hover:cursor-pointer hover:shadow-lg hover:shadow-pink-200 select-none' />
-                </Link>
-                {Object.keys(links).map(x => {
-                    return <Link href={links[x]} key={x} className='font-medium ml-4 text-xl text-pink-500 hover:text-pink-700 transition'>
-                        {x}
-                    </Link>
-                })}
-                <div className='ml-auto flex items-center'>
-                    <Link href='/login' className='font-medium text-xl text-pink-500 hover:text-pink-700 transition'>
-                        Login
-                    </Link>
-                    <span className='mx-2 text-pink-300'>|</span>
-                    <Link href='/signup' className='font-medium text-xl text-pink-500 hover:text-pink-700 transition'>
-                        Sign up
-                    </Link>
-                </div>
-            </div>
-            
-            {children}
-
-            <div className='py-8 lg:px-16 md:px-8 px-4 w-full border-t border-slate-300 mt-16'>
-                <div className='flex flex-wrap'>
-                    <div className='font-medium'>
-                        <img src='/logo.png' className='saturate-0 opacity-50' width={190} />
-                        <p className='w-[400px] text-zinc-500'>
-                            WWMS Now
-                            <br />
-                            Developed for the YPAR project
-                            <br />
-                            By Pranav Thota, Ekya Dogra, Aditya Arcot-Vantel, Kushagra Dixit, and Dillon Petagna
-                        </p>
-                    </div>
-                    <div className='ml-8 flex flex-wrap'>
-                        <div className='text-zinc-400'>
-                            <p className='text-zinc-500 font-medium'>
-                                Links
-                            </p>
-                            {Object.keys(links).map(x => {
-                                return <Link href={links[x]}>
-                                    <p className='hover:underline'>
-                                        {x}
-                                    </p>
-                                </Link>
-                            })}
-                        </div>
-                    </div>
-                    <div className='ml-8 flex flex-wrap'>
-                        <div className='text-zinc-400'>
-                            <p className='text-zinc-500 font-medium'>
-                                Miscellaneous
-                            </p>
-                            <Link href={'https://wwms.edison.k12.nj.us'}>
-                                <p className='hover:underline'>
-                                    WWMS Website
-                                </p>
-                            </Link>
-                            <Link href={'https://www.youtube.com/@woodrowwilsonmiddleschool5029'}>
-                                <p className='hover:underline'>
-                                    WWMS YouTube
-                                </p>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
+      <div className={`w-full sticky top-0 min-h-screen`}>
+        <div className="flex items-center hover:cursor-default w-full bg-pink-100 py-3 lg:px-16 md:px-8 px-4 border-b-2 border-pink-500">
+          <Link href="/">
+            <img
+              src="/logo.png"
+              alt="logo"
+              width="120"
+              className="mr-1 rounded border border-pink-500 hover:scale-[1.05] transition hover:cursor-pointer hover:shadow-lg hover:shadow-pink-200 select-none"
+            />
+          </Link>
+          {Object.keys(links).map((x) => {
+            return (
+              <Link
+                href={links[x]}
+                key={x}
+                className="font-medium ml-4 text-xl text-pink-500 hover:text-pink-700 transition"
+              >
+                {x}
+              </Link>
+            );
+          })}
+          <div className="ml-auto flex items-center">
+            <Link
+              href="/login"
+              className="font-medium text-xl text-pink-500 hover:text-pink-700 transition"
+            >
+              Login
+            </Link>
+            <span className="mx-2 text-pink-300">|</span>
+            <Link
+              href="/signup"
+              className="font-medium text-xl text-pink-500 hover:text-pink-700 transition"
+            >
+              Sign up
+            </Link>
+          </div>
         </div>
+
+        {children}
+
+        <div className="py-8 lg:px-16 md:px-8 px-4 w-full border-t border-slate-300 mt-16">
+          <div className="flex flex-wrap">
+            <div className="font-medium">
+              <img
+                src="/logo.png"
+                className="saturate-0 opacity-50"
+                width={190}
+              />
+              <p className="w-[400px] text-zinc-500">
+                WWMS Now
+                <br />
+                Developed for the YPAR project
+                <br />
+                By Pranav Thota, Ekya Dogra, Aditya Arcot-Vantel, Kushagra
+                Dixit, and Dillon Petagna
+              </p>
+            </div>
+            <div className="ml-8 flex flex-wrap">
+              <div className="text-zinc-400">
+                <p className="text-zinc-500 font-medium">Links</p>
+                {Object.keys(links).map((x) => {
+                  // eslint-disable-next-line react/jsx-key
+                  return (
+                    <Link href={links[x]}>
+                      <p className="hover:underline">{x}</p>
+                    </Link>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="ml-8 flex flex-wrap">
+              <div className="text-zinc-400">
+                <p className="text-zinc-500 font-medium">Miscellaneous</p>
+                <Link href={"https://wwms.edison.k12.nj.us"}>
+                  <p className="hover:underline">WWMS Website</p>
+                </Link>
+                <Link
+                  href={
+                    "https://www.youtube.com/@woodrowwilsonmiddleschool5029"
+                  }
+                >
+                  <p className="hover:underline">WWMS YouTube</p>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,14 +1,79 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Layout from '../../components/Layout'
-import Link from 'next/link'
+import Layout from "../../components/Layout";
 
 export default function FAQ() {
-    return (
-        <>
-            <Layout>
-                <p className={'text-pink-500'}> placeholder </p>
-            </Layout>
-        </>
-    )
+  return (
+    <>
+      {/* TODO replace placeholders with actual information */}
+
+      <Layout>
+        <div className="max-w-screen-xl p-8 mx-auto">
+          <h2 className="mb-12 text-3xl font-extrabold leading-9 text-gray-900 border-b-2 border-gray-100">
+            FAQs
+          </h2>
+          <ul className="flex flex-wrap items-start gap-8">
+            <li className="w-2/5">
+              <p className="text-lg font-medium leading-6 text-gray-900">
+                What is life?
+              </p>
+              <p className="mt-2"></p>
+              <p className="text-base leading-6 text-gray-500">
+                Explanation of life
+              </p>
+              <p />
+            </li>
+            <li className="w-2/5">
+              <p className="text-lg font-medium leading-6 text-gray-900">
+                What is life?
+              </p>
+              <p className="mt-2"></p>
+              <p className="text-base leading-6 text-gray-500">
+                Explanation of life
+              </p>
+              <p />
+            </li>
+            <li className="w-2/5">
+              <p className="text-lg font-medium leading-6 text-gray-900">
+                What is life?
+              </p>
+              <p className="mt-2"></p>
+              <p className="text-base leading-6 text-gray-500">
+                Explanation of life
+              </p>
+              <p />
+            </li>
+            <li className="w-2/5">
+              <p className="text-lg font-medium leading-6 text-gray-900">
+                What is life?
+              </p>
+              <p className="mt-2  "></p>
+              <p className="text-base leading-6 text-gray-500">
+                Explanation of life
+              </p>
+              <p />
+            </li>
+            <li className="w-2/5">
+              <p className="text-lg font-medium leading-6 text-gray-900">
+                What is life?
+              </p>
+              <p className="mt-2"></p>
+              <p className="text-base leading-6 text-gray-500">
+                Explanation of life
+              </p>
+              <p />
+            </li>
+            <li className="w-2/5">
+              <p className="text-lg font-medium leading-6 text-gray-900">
+                What is life?
+              </p>
+              <p className="mt-2"></p>
+              <p className="text-base leading-6 text-gray-500">
+                Explanation of life
+              </p>
+              <p />
+            </li>
+          </ul>
+        </div>
+      </Layout>
+    </>
+  );
 }
