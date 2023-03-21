451ed2f1a36ba9eccf962b13b994534107612bdc
awsame303 <72770606+awsame303@users.noreply.github.com>
2023-03-20 20:50:00 -0400

    restructuring for events.js

---
+++ src/pages/events/index.js
@@ -0,0 +1,11 @@
+import Layout from '../../../components/Layout'
+import EventsCard from '../../../components/EventsCard'
+export default function Events() {
+    return (
+        <>
+            <Layout>
+                <EventsCard/>
+            </Layout>
+        </>
+    )
+}
---
+++ src/pages/events/post/[id].js
@@ -0,0 +1,12 @@
+import Layout from "../../../../components/Layout";
+import "tippy.js/animations/shift-toward-subtle.css";
+
+export default function Events() {
+    return (
+        <Layout>
+            <div className="lg:px-16 md:px-8 px-4 py-16 bg-gradient-to-tr from-zinc-900 to-slate-800 text-white">
+                {/*TODO add shit to this page because it needs to look good*/}
+            </div>
+        </Layout>
+    );
+};
